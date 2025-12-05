023df7af22ed10f398ce82dc09a4b3008c589a57
Ernst <ernstamb25@gmail.com>
2025-12-05 12:34:18 +0700

    update

--- Responsi 1 Ernst/main.py
+++ Responsi 1 Ernst/main.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
     database.init_db()
     
     root = tk.Tk()
-    # Mengatur tema agar sedikit lebih modern (opsional)
+    
     try:
-        root.tk.call("source", "azure.tcl") # Baris ini abaikan jika tidak punya tema eksternal
+        root.tk.call("source", "azure.tcl") 
         root.tk.call("set_theme", "light")
     except:
         pass
